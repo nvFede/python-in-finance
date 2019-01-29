ed39609aff6715888f919ab78f9ef7a6b6b77cd3
Federico <nvfede@gmail.com>
2019-01-29 15:32:54 +0100

    Estrategia con medias moviles

--- medias_moviles.py
+++ medias_moviles.py
@@ -1,6 +1,25 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-print("spy")
-spy = pd.read_csv('SPY.csv', parse_dates=True, index_col=0)
-print("spy")
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('SPY.csv', parse_dates=True, index_col=0)
+
+short_avg = 18
+long_avg = 40
+
+signals = pd.DataFrame(index=data.index)
+signals['signal'] = 0.0
+
+signals['short_avg'] = data['Close'].rolling(short_avg).mean()
+signals['long_avg'] = data['Close'].rolling(long_avg).mean()
+
+signals['signal'][short_avg:] = np.where(signals['short_avg'][short_avg:] > signals['long_avg'][long_avg], 1.0, 0.0)
+
+signals['positions'] = signals['signal'].diff()
+signals.dropna()
+print(signals)
+#
+# data['Close'].plot()
+# signals['short_avg'].plot()
+# signals['long_avg'].plot()
+# plt.show()
