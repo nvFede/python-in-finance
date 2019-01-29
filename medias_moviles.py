2a46ac50c5cbcd58787b118592d4dc215ea932f9
Federico Noya <nvfede@gmail.com>
2019-01-29 17:03:28 +0100

    Cruce de Medias moviles

--- medias_moviles.py
+++ medias_moviles.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import seaborn as sns
 
-data = pd.read_csv('SPY.csv', parse_dates=True, index_col=0)
+sns.set()
 
+#modificamos las opciones para ver mas columnas y mas filas del dataframe
+pd.set_option('display.width', 1200)
+pd.set_option('display.max_columns', 50)
+pd.set_option('display.max_rows', 5000)
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+file = dir_path + '/SPY.csv'
+data = pd.read_csv(file, parse_dates=True, index_col=0)
+data.dropna(inplace=True)
 short_avg = 18
 long_avg = 40
 
@@ -12,14 +24,21 @@ signals['signal'] = 0.0
 
 signals['short_avg'] = data['Close'].rolling(short_avg).mean()
 signals['long_avg'] = data['Close'].rolling(long_avg).mean()
-
-signals['signal'][short_avg:] = np.where(signals['short_avg'][short_avg:] > signals['long_avg'][long_avg], 1.0, 0.0)
-
-signals['positions'] = signals['signal'].diff()
-signals.dropna()
-print(signals)
 #
-# data['Close'].plot()
-# signals['short_avg'].plot()
-# signals['long_avg'].plot()
-# plt.show()
+signals['signal'][short_avg:] = np.where(signals['short_avg'][short_avg:] > signals['long_avg'][short_avg:], 1.0, 0.0)
+
+signals['position'] = signals['signal'].diff()
+signals.dropna(inplace=True)
+print(signals)
+
+ax1 = plt.subplot()
+ax1.plot(data['Close'])
+ax1.plot(signals['short_avg'])
+ax1.plot(signals['long_avg'])
+
+# dibujar señales de compra
+ax1.plot(signals.loc[signals.position == 1.0].index, signals.short_avg[signals.position == 1.0],
+    '^', markersize=15, c='g')
+# dibujar señales de venta
+ax1.plot(signals.loc[signals.position == -1.0].index, signals.short_avg[signals.position == -1.0], '*', markersize=15, c='r')
+plt.show()
